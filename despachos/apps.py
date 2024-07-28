700b52949a7eb57b7f274a1c6dfa04ace95ccf2e
Pablo Yafac <yahu39pc@gmail.com>
2024-07-28 02:18:39 -0500

    Plantilla base de django

---
+++ despachos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DespachosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'despachos'
---
+++ marcaciones/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+# Create your views here.
+
+
+def index(request):
+    # return HttpResponse("<h1>Hello World</h1>")
+    title = "DJANGO COURSE"
+    return render(request,'index.html', {'title': title})
+
+def about(request):
+    #return HttpResponse("about")
+    username = "YAHUPC"
+    return render(request,'about.html',{'username': username})
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello %s </h1> " % username)
+
+def projects(request):
+    projects = list(Project.objects.values())
+    #return JsonResponse(projects, safe=False)
+    return render(request,'projects.html', {'projects': projects})
+
+def tasks(request):
+    # tasks = Task.objects.get(id=id)
+    # tasks = get_object_or_404(Task, id=id)
+    #return HttpResponse("tasks: %s" % tasks.title)
+    tasks = list(Task.objects.values()
+    return render(request,'tasks.html',{'tasks': tasks})
